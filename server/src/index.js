3137ccf583344f2ce1a0df1087fc538c1460c222
RatonMortal0 <taniamartinezhervasclase@gmail.com>
2026-04-14 10:13:14 +0200

    feat: configurar despliegue en Vercel

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const { PORT } = require('./config/env');
 const taskRoutes = require('./routes/task.routes');
 
@@ -9,25 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../../src')));
-
 app.use('/api/v1/tasks', taskRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../src/index.html'));
-});
-
 app.use((err, req, res, next) => {
-
   if (err.message === 'NOT_FOUND') {
     return res.status(404).json({ error: 'Recurso no encontrado' });
   }
-
-
   console.error(err);
   res.status(500).json({ error: 'Error interno del servidor' });
 });
 
-app.listen(PORT, () => {
-  console.log('Servidor corriendo en http://localhost:' + PORT);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log('Servidor corriendo en http://localhost:' + PORT);
+  });
+}
+
+module.exports = app;
